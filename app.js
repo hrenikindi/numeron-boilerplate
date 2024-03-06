b9d981d0ad15be9db5a789b588ae73bfb1e79a09
Harshith Renikindi <harshith.renikindi@kalvium.community>
2024-03-06 17:01:31 +0530

    done necessary changes in the file to make game functional

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Iteration 1: Make the Play button functional.
+var playBtn=document.querySelector('#play-button');
+
+function play(){
+    location.href="./game.html";
+}
+playBtn.addEventListener('click',play);
 // Description: When the Play button is clicked the game page should be displayed.
